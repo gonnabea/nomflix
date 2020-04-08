e7de730245e255a59653d7161d0c73f655d2b6b8
Choi Jiwon <winning1993@naver.com>
2020-04-09 04:00:49 +0900

    #3.3 GlobalStyles and Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,9 +4,12 @@ import Home from "Routes/Home";
 import Detail from "Routes/Detail";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
+
 
 export default () => (
     <Router>
+        <Header/>
         <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/detail" exact component={Detail} />
